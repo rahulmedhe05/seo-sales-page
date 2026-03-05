884da11c42e7cae953cd377f34a1fab8a8023ba3
mysafawale-ai <mysafawale@gmail.com>
2026-03-06 01:14:51 +0530

    Move Meta Pixel to analytics-tracker component

--- components/analytics-tracker.tsx
+++ components/analytics-tracker.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
+import Script from "next/script"
 import { startSession, heartbeat, endSession, trackPageView, getCurrentSessionId } from "@/lib/analytics"
 
 export function AnalyticsTracker() {
@@ -53,6 +54,32 @@ export function AnalyticsTracker() {
     }
   }, [])
 
-  // This component doesn't render anything
-  return null
+  return (
+    <>
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1484980699653222');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1484980699653222&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+    </>
+  )
 }
